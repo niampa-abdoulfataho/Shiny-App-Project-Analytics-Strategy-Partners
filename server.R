0fda92cac34892a39d7de661c6c61831e78ef240
yamsaid <saidouyameogo3@gmail.com>
2026-03-19 06:26:30 +0000

    l'interface v1

--- server.R
+++ server.R
@@ -9,20 +9,88 @@
 
 library(shiny)
 
+"==========================================================================="
+"======         Données pour le message ===================================="
+"==========================================================================="
+messageData <- reactive(
+   data.frame(
+   from    = c("Alice", "Bob"),
+   message = c("Rapport prêt", "Réunion à 14h ?"),
+   time    = c("Il y a 5 min", "Il y a 1h"),
+   stringsAsFactors = FALSE
+))
+
+"=============================================================================="
+
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+        set.seed(122)
+        histdata <- rnorm(500)
+        output$plot1 <- renderPlot({
+            data <- histdata[seq_len(input$slider)]
+            hist(data)
+        })
+        
+        # Message pour afficher dans le header
+        messageData <- reactive(
+           data.frame(
+              from    = c("Saïdou YAMEOGO", "A. Fatah NIAMPA"),
+              message = c("Rapport prêt", "Réunion à 14h ?"),
+              time    = c("Il y a 5 min", "Il y a 1h"),
+              stringsAsFactors = FALSE
+           ))
+        # Le contenu de menu message
+        output$messageMenu <- renderMenu({
+           # Code to generate each of the messageItems here, in a list. This assumes
+           # that messageData is a data frame with two columns, 'from' and 'message'.
+           msgs <- lapply(seq_len(nrow(messageData())), function(i) {
+              messageItem(
+                 from    = messageData()$from[i],
+                 message = messageData()$message[i],
+                 time = messageData()$message[i]
+              )
+           })
+           
+           # This is equivalent to calling:
+           dropdownMenu(type = "messages", .list = msgs)
+        })
+        
+        # Message pour les notifications dans le header
+        notificationData <- reactive(
+           data.frame(
+              text    = c(
+                 "5 new users today", 
+                 "12 items delivered", 
+                 "Server load at 86%"
+                 ),
+              icon = c("users", "truck", "exclamation-triangle"),
+              status = c("primary", "success","warning"),
+              stringsAsFactors = FALSE
+           ))
+        
+        # Le contenu de menu notification 
+        output$notificationMenu <- renderMenu({
+           # Code to generate each of the messageItems here, in a list. This assumes
+           # that messageData is a data frame with two columns, 'from' and 'message'.
+           msgs <- lapply(seq_len(nrow(notificationData())), function(i) {
+              notificationItem(
+                 text    = notificationData()$text[i],
+                 icon = icon(notificationData()$icon[i]),
+                 status = notificationData()$status[i]
+              )
+           })
+           
+           # This is equivalent to calling:
+           dropdownMenu(type = "notifications", .list = msgs)
+        })
+        
+        #connexion deconnexion
+        output$userpanel <- renderUI({
+           # session$user is non-NULL only in authenticated sessions
+           if (!is.null(session$user)) {
+              sidebarUserPanel(
+                 span("Logged in as ", session$user),
+                 subtitle = a(icon("sign-out"), "Logout", href="__logout__"))
+           }
+        })
 }
--- ui.R
+++ ui.R
@@ -1,33 +1,91 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(shinydashboard) # <-- Change this line to: library(semantic.dashboard)
 
-# Define UI for application that draws a histogram
-fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+#Le header 
+pageHeader <- dashboardHeader(
+    title = "Basic dashboard",
+    dropdownMenuOutput("messageMenu"),
+    dropdownMenuOutput("notificationMenu"),
+    dropdownMenu(type = "tasks", 
+                 badgeStatus = "success",
+                 taskItem(value = 90, color = "green",
+                          "Documentation"
+                 ),
+                 taskItem(value = 17, color = "aqua",
+                          "Project X"
+                 ),
+                 taskItem(value = 75, color = "yellow",
+                          "Server deployment"
+                 ),
+                 taskItem(value = 80, color = "red",
+                          "Overall project"
+                 )
     )
 )
+
+
+# La barre latérale 
+pageSiderbar <- dashboardSidebar(
+    sidebarMenu(
+        sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                          label = "Search..."),
+        # Custom CSS to hide the default logout panel
+        tags$head(tags$style(HTML('.shiny-server-account { display: none; }'))),
+        # The dynamically-generated user panel
+        uiOutput("userpanel"),
+        menuItem(tabName = "home", text = "Home", icon = icon("home")),
+        menuItem(tabName = "another", text = "Another Tab", icon = icon("heart")),
+        menuItem("Source code", icon = icon("file-code-o"), 
+                 href = "https://github.com/rstudio/shinydashboard/",
+                 badgeLabel = "link", badgeColor = "blue"),
+        menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+                 badgeLabel = "new", badgeColor = "green")
+    )
+)
+
+
+# Le corps de la page
+pageBody <- dashboardBody(
+    fluidRow(
+        box(title = "Histogram",
+            plotOutput("plot1"), 
+            status = "success",
+            solidHeader = TRUE,
+            collapsible = TRUE,
+            background = "maroon"
+            ),
+        
+        box(
+            "Box content here", br(), "More box content",
+            sliderInput("slider", "Slider input:", 1, 100, 50),
+            textInput("text", "Text input:")
+        )
+    ),
+    fluidRow(
+        # A static valueBox
+        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
+        
+        # Dynamic valueBoxes
+        valueBoxOutput("progressBox"),
+        
+        valueBoxOutput("approvalBox")
+    ),
+    fluidRow(
+        # Clicking this will increment the progress amount
+        box(width = 4, actionButton("count", "Increment progress"))
+    ),
+    tags$head(
+        tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    )
+)
+    
+
+
+
+#===============================================================================
+# Le tableau de bord 
+dashboardPage(pageHeader, pageSiderbar, pageBody)
+
+#===============================================================================
+
